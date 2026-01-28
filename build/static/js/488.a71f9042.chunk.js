ae9ab1e6580dd3519d3f5dbcaa1381c2627846de
exael2000 <exaelyoandavid@gmail,com>
2026-01-28 17:42:27 -0600

    Fix: Actualizar build con archivos JS/CSS correctos

---
+++ build/static/js/488.a71f9042.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktienda_abarrotes_react=self.webpackChunktienda_abarrotes_react||[]).push([[488],{488(e,a,t){new Map,new Date}}]);
+//# sourceMappingURL=488.a71f9042.chunk.js.map
